b85521a8938f292ca1990b86833e0f49be990163
Adam Harner <adamj.harner@gmail.com>
2020-07-09 14:39:02 -0400

    created solution

---
+++ mediums/coin_change_combos.py
@@ -0,0 +1,18 @@
+#Usefull video explaining algo used. https://www.youtube.com/watch?v=jaNZ83Q3QGc
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        combinations = (amount+1) * [0]
+        
+        combinations[0] = 1
+        for coin in coins:
+            for i in range(len(combinations)):
+                if i >= coin:
+                    combinations[i] += combinations[i-coin]
+                    
+                    
+                    
+        return combinations[amount]
+        
+       
